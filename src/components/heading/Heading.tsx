a691cdc2be0043843ddcb022549693f1f58a6690
ValpSerg <Ghostnord@gmail.com>
2024-04-09 14:38:46 +0300

    Heading fade-up

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -10,16 +10,18 @@ interface HeadingProps {
 const Heading: React.FC<HeadingProps> = ({ variant = "dark", heading }) => {
   return (
     <>
-      <Text
-        variant={variant}
-        className=" uppercase"
-        fontFamily="Montserrat"
-        title={heading.title}
-        size="xl"
-        bold
-      />
+      <div data-aos="fade-up">
+        <Text
+          variant={variant}
+          className=" uppercase"
+          fontFamily="Montserrat"
+          title={heading.title}
+          size="xl"
+          bold
+        />
+      </div>
 
-      <div className="flex flex-col gap-2 items-center ">
+      <div data-aos="fade-up" className="flex flex-col gap-2 items-center ">
         {heading.paragraphs &&
           heading.paragraphs.map((paragraph) => (
             <div key={paragraph.id}>
